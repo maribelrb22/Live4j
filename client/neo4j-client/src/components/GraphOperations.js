6fd8402479b3f4bd512f06e374765d0de6c6b565
Maribel Ramos <maribelrb.5@gmail.com>
2022-05-08 02:18:43 +0200

    Adds some improvements

--- client/neo4j-client/src/components/GraphOperations.js
+++ client/neo4j-client/src/components/GraphOperations.js
@@ -6,6 +6,7 @@ import { InputTextarea } from 'primereact/inputtextarea';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { MultiSelect } from 'primereact/multiselect';
+import { Calendar } from 'primereact/calendar';
 
 const GraphOperations = () => {
     const [inputQuery, setInputQuery] = useState('');
@@ -15,12 +16,42 @@ const GraphOperations = () => {
     const [voteAverage, setVoteAverage] = useState('');
     const [genre, setGenre] = useState('');
     const [selectedGenres, setSelectedGenres] = useState(null);
+    const [formErrors, setFormErrors] = useState({});
+
+    const yearRange = "1800:" + new Date().getFullYear();
 
     const genres = [
         { name: 'Acción' },
         { name: 'Aventura' },
         { name: 'Crimen' },
     ];
+
+    const validateFilm = () => {
+        const errors = {}
+
+        if (!title) errors.title = "El título es requerido"
+        if (!language) errors.language = "El idioma es requerido"
+        if (!releaseDate) errors.releaseDate = "La fecha de estreno es requerida"
+        if (!voteAverage) errors.voteAverage = "La puntuación es requerida"
+
+        setFormErrors(errors)
+    }
+
+    const validateGenre = () => {
+        const errors = {}
+        if (!genre) errors.genre = "El género es requerido"
+        setFormErrors(errors)
+    }
+
+    const validateQuery = () => {
+        const errors = {}
+        if (!inputQuery) errors.inputQuery = "La consulta es requerida"
+        setFormErrors(errors)
+    }
+
+    const getFieldError = (field) => {
+        return formErrors[field] && <span className="errorMessage">{formErrors[field]}</span>
+    }
 
     return (
         <Card className="w-full p-operation-panel" title="Películas">
@@ -31,37 +62,41 @@ const GraphOperations = () => {
                             <i className="pi pi-plus-circle mr-3"></i>
                             <span>Crear película</span>
                         </React.Fragment>}>
-                    <div className="p-inputgroup mb-2">
+                    <div className="p-inputgroup">
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-id-card"></i>
                         </span>
                         <InputText placeholder='Título de la película' value={title} onChange={(e) => setTitle(e.target.value)} />
                     </div>
-                    <div className="p-inputgroup mb-2">
+                    {getFieldError("title")}
+                    <div className="p-inputgroup mt-2">
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-globe"></i>
                         </span>
                         <InputText className='w-full' placeholder='Idioma original' value={language} onChange={(e) => setLanguage(e.target.value)} />
                     </div>
-                    <div className="p-inputgroup mb-2">
+                    {getFieldError("language")}
+                    <div className="p-inputgroup mt-2">
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-calendar"></i>
                         </span>
-                        <InputText className='w-full' placeholder='Fecha de estreno' value={releaseDate} onChange={(e) => setReleaseDate(e.target.value)} />
+                        <Calendar className='w-full' placeholder='Fecha de estreno' value={releaseDate} onChange={(e) => setReleaseDate(e.value)} yearNavigator monthNavigator yearRange={yearRange} />
                     </div>
-                    <div className="p-inputgroup mb-2">
+                    {getFieldError("releaseDate")}
+                    <div className="p-inputgroup mt-2">
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-star"></i>
                         </span>
                         <InputText className='w-full' placeholder='Puntuación' value={voteAverage} onChange={(e) => setVoteAverage(e.target.value)} />
                     </div>
-                    <div className="p-inputgroup mb-2">
+                    {getFieldError("voteAverage")}
+                    <div className="p-inputgroup mt-2">
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-tag"></i>
                         </span>
                         <MultiSelect value={selectedGenres} options={genres} onChange={(e) => setSelectedGenres(e.value)} optionLabel="name" optionValue="name" placeholder="Selecciona géneros" />
                     </div>
-                    <Button label="Crear" className='w-full' icon="pi pi-plus" onClick={() => console.log("pelicula creada")} />
+                    <Button label="Crear" className='w-full mt-3' icon="pi pi-plus" onClick={validateFilm} />
                 </AccordionTab>
                 <AccordionTab
                     header={
@@ -69,13 +104,14 @@ const GraphOperations = () => {
                             <i className="pi pi-plus-circle mr-3"></i>
                             <span>Crear género</span>
                         </React.Fragment>}>
-                    <div className="p-inputgroup mb-2">
+                    <div className="p-inputgroup">
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-tag"></i>
                         </span>
                         <InputText className='w-full' placeholder='Género' value={genre} onChange={(e) => setGenre(e.target.value)} />
                     </div>
-                    <Button label="Crear" className='w-full' icon="pi pi-plus" onClick={() => console.log("genero creada")} />
+                    {getFieldError("genre")}
+                    <Button label="Crear" className='w-full mt-3' icon="pi pi-plus" onClick={validateGenre} />
 
                 </AccordionTab>
                 <AccordionTab
@@ -85,8 +121,9 @@ const GraphOperations = () => {
                             <span>Haz tu propia consulta</span>
                         </React.Fragment>}>
                     <div className="flex flex-column">
-                        <InputTextarea className="w-full mb-3" value={inputQuery} onChange={(e) => setInputQuery(e.target.value)} rows={5} />
-                        <Button label="Enviar" icon="pi pi-send" onClick={() => console.log("consulta enviada")} />
+                        <InputTextarea className="w-full" value={inputQuery} onChange={(e) => setInputQuery(e.target.value)} rows={5} />
+                        {getFieldError("inputQuery")}
+                        <Button className="mt-3" label="Enviar" icon="pi pi-send" onClick={validateQuery} />
                     </div>
                 </AccordionTab>
             </Accordion>
--- client/neo4j-client/src/components/Graphs.js
+++ client/neo4j-client/src/components/Graphs.js
@@ -4,45 +4,54 @@ import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { MultiSelect } from 'primereact/multiselect';
+import { Calendar } from 'primereact/calendar';
 import "../css/Graphs.css";
+import initialData from "../initialData.json";
 
 const Graphs = () => {
-    const [nodeSelected, setNodeSelected] = useState(false);
+    const [filmSelected, setFilmSelected] = useState(false);
     const [edgeSelected, setEdgeSelected] = useState(false);
     const [idSelectedNode, setIdSelectedNode] = useState(false);
     const [idSelectedEdge, setIdSelectedEdge] = useState(false);
 
+    const yearRange = "1800:" + new Date().getFullYear();
+    const genres = [
+        { id: 24, label: 'Action' },
+        { id: 25, label: 'Adventure' },
+        { id: 26, label: 'Science Fiction' },
+        { id: 27, label: 'Crime' },
+        { id: 28, label: 'Mystery' },
+        { id: 29, label: 'Thriller' },
+    ]
+
     const [graph] = useState({
-        nodes: [
-            { id: 1, label: "Node 1", title: "node 1" },
-            { id: 2, label: "Node 2", title: "node 2" },
-            { id: 3, label: "Node 3", title: "node 3" },
-            { id: 4, label: "Node 4", title: "node 4" },
-            { id: 5, label: "Node 5", title: "node 5" }
-        ],
+        nodes: (initialData.map(node => ({
+            id: node.id,
+            label: node.name,
+        }))).concat(genres),
+
         edges: [
             { id: 1, from: 1, to: 2, label: "arista 1" },
             { id: 2, from: 1, to: 3, label: "arista 2" },
             { id: 3, from: 2, to: 4, label: "arista 3" },
             { id: 4, from: 2, to: 5, label: "arista 4" }
         ]
-    })
+    });
 
     const options = {
         layout: {
             hierarchical: false
         },
         edges: {
-            color: "#000000"
+            color: "#000000",
         },
-        height: "500px"
     };
 
     const events = {
         select: ({ nodes, edges }) => {
             if (nodes.length !== 0) {
                 setIdSelectedNode(nodes)
-                setNodeSelected(true)
+                setFilmSelected(true)
             }
 
             if (nodes.length === 0 && edges.length !== 0) {
@@ -52,7 +61,21 @@ const Graphs = () => {
         },
     };
 
-    //TODO: Differentiate between movies and genres
+    /*
+    TODO: Differentiate between movies and genres
+
+    <Dialog header="¿Quieres eliminar el género?" className="w-3" visible={genreSelected} onHide={() => setGenreSelected(false)}>
+        <div className="grid w-full m-auto">
+            <div className="col-12 md:col-6">
+                <Button label="Cancelar" className="w-full h-full" icon="pi pi-times" onClick={() => console.log("cancelar eliminado")} />
+            </div>
+            <div className="col-12 md:col-6">
+                <Button label="Eliminar" className="p-button-danger w-full h-full" icon="pi pi-minus-circle" onClick={() => console.log("genero eliminado")} />
+            </div>
+        </div>
+    </Dialog>
+    */
+
     return (
         <div>
             <Graph
@@ -61,46 +84,56 @@ const Graphs = () => {
                 events={events}
             />
 
-            <Dialog header="¿Qué quieres hacer?" className="w-4" visible={nodeSelected} onHide={() => setNodeSelected(false)}>
-                <div className="text-center">
-                    <div className="p-inputgroup mb-2">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-id-card"></i>
-                        </span>
-                        <InputText placeholder='Título de la película' />
+            <Dialog header="Propiedades de la película" className="w-4" visible={filmSelected} onHide={() => setFilmSelected(false)}>
+
+                <div className="p-inputgroup mb-2">
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-id-card"></i>
+                    </span>
+                    <InputText placeholder='Título de la película' />
+                </div>
+                <div className="p-inputgroup mb-2">
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-globe"></i>
+                    </span>
+                    <InputText className='w-full' placeholder='Idioma original' />
+                </div>
+                <div className="p-inputgroup mb-2">
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-calendar"></i>
+                    </span>
+                    <Calendar className='w-full' placeholder='Fecha de estreno' yearNavigator monthNavigator yearRange={yearRange} />
+                </div>
+                <div className="p-inputgroup mb-2">
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-star"></i>
+                    </span>
+                    <InputText className='w-full' placeholder='Puntuación' />
+                </div>
+                <div className="p-inputgroup mb-2">
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-tag"></i>
+                    </span>
+                    <MultiSelect className="left-auto" placeholder="Selecciona géneros" />
+                </div>
+                <div className="grid w-full m-auto">
+                    <div className="col-12 md:col-6">
+                        <Button label="Editar" icon="pi pi-pencil" className="w-full h-full" onClick={() => console.log("nodo editado")} />
                     </div>
-                    <div className="p-inputgroup mb-2">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-globe"></i>
-                        </span>
-                        <InputText className='w-full' placeholder='Idioma original' />
+                    <div className="col-12 md:col-6">
+                        <Button label="Eliminar" className="p-button-danger w-full h-full" icon="pi pi-minus-circle" onClick={() => console.log("nodo eliminado")} />
                     </div>
-                    <div className="p-inputgroup mb-2">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-calendar"></i>
-                        </span>
-                        <InputText className='w-full' placeholder='Fecha de estreno' />
-                    </div>
-                    <div className="p-inputgroup mb-2">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-star"></i>
-                        </span>
-                        <InputText className='w-full' placeholder='Puntuación' />
-                    </div>
-                    <div className="p-inputgroup mb-2">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-tag"></i>
-                        </span>
-                        <MultiSelect placeholder="Selecciona géneros" />
-                    </div>
-                    <Button label="Editar" icon="pi pi-pencil" onClick={() => console.log("nodo editado")} />
-                    <Button label="Eliminar" className="ml-3 p-button-danger" icon="pi pi-minus-circle" onClick={() => console.log("nodo eliminado")} />
                 </div>
             </Dialog>
-            <Dialog header="¿Quieres eliminar la relación?" className="w-4" visible={edgeSelected} onHide={() => setEdgeSelected(false)}>
-                <div className="text-center">
-                    <Button label="Eliminar" className="p-button-danger" icon="pi pi-minus-circle" onClick={() => console.log("arista eliminada")} />
-                    <Button label="Cancelar" className="ml-3" icon="pi pi-times" onClick={() => console.log("cancelar eliminado")} />
+
+            <Dialog header="¿Quieres eliminar la relación?" className="w-3" visible={edgeSelected} onHide={() => setEdgeSelected(false)}>
+                <div className="grid w-full m-auto">
+                    <div className="col-12 md:col-6">
+                        <Button label="Cancelar" className="w-full h-full" icon="pi pi-times" onClick={() => console.log("cancelar eliminado")} />
+                    </div>
+                    <div className="col-12 md:col-6">
+                        <Button label="Eliminar" className="p-button-danger w-full h-full" icon="pi pi-minus-circle" onClick={() => console.log("arista eliminada")} />
+                    </div>
                 </div>
             </Dialog>
         </div>
